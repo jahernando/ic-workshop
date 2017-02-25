74162be9b7e05151556e3257d2d50706a5cf5576
Jose A. Hernando <hernando.joseangel@gmail.com>
2017-02-25 11:08:32 +0100

    Create Borges branch and add somme text.

---
+++ .gitignore.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 11:21:51 2017
+
+@author: hernando
+"""
+
+*.pyc
+*.py~
---
+++ shakespeare.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 25 11:38:39 2017
+
+@author: hernando
+"""
+
+print("To be or not to be, this is the question!")
+
+print("Life is a tale told by an idiot!")
